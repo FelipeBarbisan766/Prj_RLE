84a37136e25480cf6911e54cf39bf63bad85f067
FelipeBarbisan766 <felipebarbisan76@gmail.com>
2024-07-13 16:01:54 -0300

    updates

--- Professor/pageProfessor.php
+++ Professor/pageProfessor.php
@@ -7,13 +7,15 @@
     <title>Document</title>
 </head>
 
-<body>
-    <?php
+<?php
     include_once ("../navbar.php");
-    ?>
-    <a href="">Seu Cronograma</a>
-    <a href="">Historico de Reserva</a>
-    <a href="">Mudar senha</a>
+?>
+<body>
+<a class="btn btn-primary" href="../"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left-circle" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z"/></svg> voltar</a>
+    
+    <a href="crono_prof.php" class="btn btn-primary">Meu Cronograma</a>
+    <a href="reservas.php" class="btn btn-primary">Reservas</a>
+    <a href="altSenha.php" class="btn btn-primary">Mudar senha</a>
 
 </body>
 
