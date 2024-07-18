a90cd2c3bd87f3dad0d5aae6d7a24538cc1827e1
FelipeBarbisan766 <felipebarbisan76@gmail.com>
2024-07-17 21:14:42 -0300

    update

--- Calendario/dia.php
+++ Calendario/dia.php
@@ -6,8 +6,21 @@ date_default_timezone_set('America/Sao_Paulo');
 <div class="row g-0 text-center">
     <div class="col-6 col-md-4">
         <form action="" method="get">
-            <label for="data">Data:</label>
-            <input type="date" name="data" id="data">
+            <label for="data" class="form-label">Data:</label>
+            <input type="date" name="data" id="data" <?php if (isset($_GET['data'])) {echo 'value="' . $_GET['data'] . '"';} ?>>
+            <br>
+            <label for="lab" >laboratorio:</label>
+            <select name="lab" id="lab" required>
+            <?php
+            include_once ("../conexao.php");
+            $slq = mysqli_query($conexao, "SELECT * FROM laboratorio");
+            while ($labs = mysqli_fetch_array($slq)) {
+                if ($labs['lab_isActive'] == true) { ?>
+                    <option value="<?php echo $labs['lab_cod']; ?>"><?php echo $labs['lab_nome']; ?></option>
+                <?php }
+            }
+            ; ?>
+            </select>
             <input type="submit" value="Continuar">
         </form>
     </div>
@@ -24,15 +37,23 @@ date_default_timezone_set('America/Sao_Paulo');
             $arrydata = getdate();
             $sem = $arrydata['wday'];
         }
-        $slq_reserva = mysqli_query($conexao, "SELECT r.res_aula as aula,r.res_desc as descr,r.res_isActive as active, p.prof_nome as prof FROM reserva as r INNER JOIN professor as p on r.prof_cod=p.prof_cod INNER JOIN laboratorio as l on r.lab_cod=l.lab_cod WHERE r.res_data = '$data' ORDER BY r.res_aula ASC");
-        $slq_cronograma = mysqli_query($conexao, "SELECT c.cro_aula as aula,c.cro_desc as descr,c.cro_isActive as active, p.prof_nome as prof FROM cronograma as c INNER JOIN laboratorio as l on c.lab_cod=l.lab_cod INNER JOIN professor as p on c.prof_cod=p.prof_cod WHERE c.cro_sem = '$sem' ORDER BY c.cro_aula ASC");
+        if (isset($_GET['lab'])) {
+            $lab = $_GET['lab'];
+        } else {
+            $labs = mysqli_fetch_array(mysqli_query($conexao, "SELECT * FROM laboratorio"));//tentar sumir com isso pq repete 
+            $lab = $labs['lab_cod'];
+        }
+        $labnome = mysqli_fetch_array(mysqli_query($conexao, "SELECT * FROM laboratorio WHERE lab_cod='$lab'"));
+        $nomelab = $labnome["lab_nome"];
+        $slq_reserva = mysqli_query($conexao, "SELECT r.res_aula as aula,r.res_desc as descr,r.res_isActive as active, p.prof_nome as prof FROM reserva as r INNER JOIN professor as p on r.prof_cod=p.prof_cod INNER JOIN laboratorio as l on r.lab_cod=l.lab_cod WHERE r.res_data = '$data' AND l.lab_cod = '$lab' ORDER BY r.res_aula ASC");
+        $slq_cronograma = mysqli_query($conexao, "SELECT c.cro_aula as aula,c.cro_desc as descr,c.cro_isActive as active, p.prof_nome as prof FROM cronograma as c INNER JOIN laboratorio as l on c.lab_cod=l.lab_cod INNER JOIN professor as p on c.prof_cod=p.prof_cod WHERE c.cro_sem = '$sem' AND l.lab_cod = '$lab' ORDER BY c.cro_aula ASC");
         while ($reserva = mysqli_fetch_array($slq_reserva)) {
             $aula1 = null;
             $aula2 = null;
             $aula3 = null;
             $aula4 = null;
             $aula5 = null;
-            $aula6 = null;    
+            $aula6 = null;
             switch ($reserva["aula"]) {
                     case "1":
                         $aula1 = ['desc' => $reserva['descr'],'prof' => $reserva['prof']];
@@ -83,7 +104,7 @@ date_default_timezone_set('America/Sao_Paulo');
             }
 
         ; ?>
-        <h1><?php echo (new DateTime($data))->format('d/m/Y').' - '. $arrydata['weekday']; ?></h1> <!-- trocar para portugues o nome da semana ! -->
+        <h1><?php echo (new DateTime($data))->format('d/m/Y').' - '. $arrydata['weekday'].' - '.$nomelab; ?></h1> <!-- trocar para portugues o nome da semana ! -->
         <a class="list-group-item list-group-item-action" <?php if(!isset($aula1)){echo 'href="../Reserva/formReserva.php?data='.$data.'&aula=1"';}else{echo 'disabled';}?>>1ºAula - <?php if(!isset($aula1)){echo "livre";}else{echo $aula1['desc'];}if(!isset($aula1)){echo "Nenhum";}else{echo '- Professor(a)'.$aula1['prof'];}  ?></a>
         <a class="list-group-item list-group-item-action" <?php if(!isset($aula1)){echo 'href="../Reserva/formReserva.php?data='.$data.'&aula=2"';}else{echo 'disabled';}?>>2ºAula - <?php if(!isset($aula2)){echo "livre";}else{echo $aula2['desc'];}if(!isset($aula2)){echo "Nenhum";}else{echo '- Professor(a)'.$aula2['prof'];}  ?></a>
         <a class="list-group-item list-group-item-action" <?php if(!isset($aula1)){echo 'href="../Reserva/formReserva.php?data='.$data.'&aula=3"';}else{echo 'disabled';}?>>3ºAula - <?php if(!isset($aula3)){echo "livre";}else{echo $aula3['desc'];}if(!isset($aula3)){echo "Nenhum";}else{echo '- Professor(a)'.$aula3['prof'];}  ?></a>
