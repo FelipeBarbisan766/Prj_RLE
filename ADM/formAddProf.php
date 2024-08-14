06cd81d014d6734da62a7e97a5ffac6883a83784
felipegalati <felipegalati20@gmail.com>
2024-08-14 15:42:39 -0300

    botão voltar

--- ADM/formAddProf.php
+++ ADM/formAddProf.php
@@ -23,9 +23,15 @@ include_once ("../navbar2.php");
         
         <input type="submit" class="btn btn-primary">
     </form> -->
+    <div class="px-4 mx-auto max-w-screen-xl ">
+    <a href="../index.php" class="my-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">         
+        <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 8 14">    
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 1 1.3 6.326a.91.91 0 0 0 0 1.348L7 13"/>
+        </svg>
+    </a>
+    </div>
 
-
-    <h1 class="mb-4 text-xs text-center mt-10 font-extrabold tracking-tight leading-none text-gray-900 md:text-3xl lg:text-4xl dark:text-white">Adicionar Professor</h1>
+    <h1 class="mb-4 text-3xl text-center mt-2 font-extrabold tracking-tight leading-none text-gray-900 md:text-3xl lg:text-4xl dark:text-white">Adicionar Professor</h1>
 
     <form class="max-w-sm mx-auto" action="addProf.php" method="POST">
     <div class="mb-5">
