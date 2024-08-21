773906aadac7bc2be098aa5f95d1445d309fecc5
felipegalati <felipegalati20@gmail.com>
2024-08-21 12:34:47 -0300

    jeito do carlao

--- ADM/testeparametro.php
+++ ADM/testeparametro.php
@@ -1,14 +1,14 @@
 <?php
 include('../conexao.php');
 include_once ('../protectCode.php');
-$cod = $_POST['cod'];
+$cod = $_GET['cod'];
 $isActive = false;
 
 $sql = mysqli_query($conexao,"UPDATE professor SET prof_isActive='$isActive' WHERE prof_cod='$cod'");
 
-if($sql){
-    // header('Location:lista.php');
-}else{
-    echo "Erro no Insert";
-}
+// if($sql){
+//     // header('Location:lista.php');
+// }else{
+//     echo "Erro no Insert";
+// }
 ?>
