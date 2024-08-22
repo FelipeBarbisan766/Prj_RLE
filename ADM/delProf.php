198b12f9d93c3891dfe6469bd6b6cacb641c69c5
felipegalati <felipegalati20@gmail.com>
2024-08-22 16:15:48 -0300

    updates

--- ADM/delProf.php
+++ ADM/delProf.php
@@ -7,7 +7,7 @@ $isActive = false;
 $sql = mysqli_query($conexao,"UPDATE professor SET prof_isActive='$isActive' WHERE prof_cod='$cod'");
 
 if($sql){
-    header('Location:lista.php');
+    //  header('Location:lista.php');
 }else{
     echo "Erro no Insert";
 }
