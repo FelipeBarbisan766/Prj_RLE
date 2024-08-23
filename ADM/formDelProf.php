be8279cc2ef47a9079e2179d30ceb0a0d24cc591
felipegalati <felipegalati20@gmail.com>
2024-08-23 20:57:48 -0300

    visual update botão voltar

--- ADM/formDelProf.php
+++ ADM/formDelProf.php
@@ -5,7 +5,7 @@ include_once ('protectAdm.php');
 
 <div class="container mx-auto px-4">
 
-    <div class="px-4 mx-auto max-w-screen-xl ">
+    <div class="mx-auto max-w-screen-xl ">
         <a href="pageControl.php" class="my-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">         
         <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 8 14">    
           <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 1 1.3 6.326a.91.91 0 0 0 0 1.348L7 13"/>
--- ADM/lista.php
+++ ADM/lista.php
@@ -7,9 +7,9 @@
 <div class="container mx-auto px-4">
 
 <a href="pageControl.php" class="my-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-    <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 8 14">
-        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 1 1.3 6.326a.91.91 0 0 0 0 1.348L7 13"/>
-    </svg>    
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+    <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+    </svg>
 </a>
 
 <div class="relative overflow-x-auto sm:rounded-lg">
@@ -37,7 +37,12 @@
                             <td class="px-6 py-4"><?php echo $prof['prof_cod']; ?></td>
                             <td class="px-6 py-4"><?php echo $prof['prof_nome']; ?></td>
                             <td class="px-6 py-4"><?php echo $prof['prof_cargo']; ?></td>
-                            <td class="px-6 py-4"><button type="button" data-modal-target="popup-modal" data-modal-toggle="popup-modal" data-modal-codeProf="<?php echo $prof['prof_cod'];?>" data-modal-name="<?php echo $prof['prof_nome'];?>" class="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">Deletar</button></td> 
+                            <td class="px-6 py-4"><button type="button" data-modal-target="popup-modal" data-modal-toggle="popup-modal" data-modal-codeProf="<?php echo $prof['prof_cod'];?>" data-modal-name="<?php echo $prof['prof_nome'];?>" class="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+</svg>
+
+                            </button></td> 
                         </tr>
                     <?php }
                 }
