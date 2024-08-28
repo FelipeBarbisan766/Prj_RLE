e77616f706b2a32520155b09cd51f2ce68b2e344
FelipeBarbisan766 <felipebarbisan76@gmail.com>
2024-08-28 17:36:13 -0300

    AltLab

---
+++ ADM/Lab/altLab.php
@@ -0,0 +1,83 @@
+<?php
+include_once ("../../navbar2.php");
+include_once ('../protectAdm.php');
+?>
+<div class="">
+    
+    <div class="px-4 mx-auto max-w-screen-xl ">
+        <a href="pageLab.php" class="my-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">         
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+        </svg>
+        </a>
+    </div>
+    <?php if(!isset($_GET['lab'])) { ?>
+
+    <h1 class="mb-4 text-3xl text-center mt-2 font-extrabold tracking-tight leading-none text-gray-900 md:text-3xl lg:text-4xl dark:text-white">Editar Laboratorio</h1>
+
+    <form class="max-w-sm mx-auto" action="" method="GET">
+        <div class="mb-5">
+            <label for="lab" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Local</label>
+            <select id="lab" name="lab" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <?php
+                $slq = mysqli_query($conexao, "SELECT * FROM laboratorio");
+                while ($lab = mysqli_fetch_array($slq)) {
+                    if ($lab['lab_isActive'] == true) { ?>
+                        <option value="<?php echo $lab['lab_cod']; ?>"><?php echo $lab['lab_nome']; ?></option>
+                        <?php }
+                }
+                ; ?>
+            </select>
+        </div>
+        <button class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Selecionar</button>
+    </form>
+
+    
+    <?php }else{?>
+        
+        <form class="max-w-sm mx-auto" action="" method="POST">
+        <div class="mb-5 mt-2">
+                <?php
+                $cod = $_GET['lab'];
+                $slq = mysqli_query($conexao, 'SELECT * FROM laboratorio WHERE lab_cod=' . $cod . '');
+                $lab = mysqli_fetch_array($slq);
+                echo '<input type="hidden" name="cod" value="'.$cod.'">';
+                ?>
+                <label for="nome" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nome do Local</label>
+                <input value="<?php echo $lab['lab_nome']; ?>" type="text" id="nome" name="nome" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Nome" required>
+            </div>
+            <div class="mb-5">
+                <label for="desc" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Descrição do Local</label>
+                <input value="<?php echo $lab['lab_desc']; ?>" type="text" id="desc" name="desc" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            </div>
+            
+            <div class="flex-row mb-5 sm:space-x-2">    
+                    <button href="pageLab.php" class="mb-2 text-white bg-danger-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">Cancelar</button>
+                    <button type="submit" class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Confirmar</button>
+            </div>
+        </div>
+        </form>
+
+    <?php } ?>    
+</div>
+
+<?php
+if(isset($_POST['cod'])){
+include('../../conexao.php');
+$cod = $_POST["cod"];
+$nome = $_POST['nome'];
+$desc = $_POST['desc'];
+
+$sql = mysqli_query($conexao,"UPDATE laboratorio SET lab_nome='$nome', lab_desc='$desc' WHERE lab_cod='$cod'");
+
+if($sql){
+    echo "<script> window.location.href='pageLab.php'</script>";
+}else{
+    echo "Erro no alter";
+}
+}
+?>
+
+
+</body>
+</html>
