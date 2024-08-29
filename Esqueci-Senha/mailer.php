938b04c8be6c19045d2b7aad4d758f7304d7679a
Eduardo Vizicato <eduardovizicato@gmail.com>
2024-08-29 16:22:45 -0300

    commit

--- Esqueci-Senha/mailer.php
+++ Esqueci-Senha/mailer.php
@@ -1,17 +1,20 @@
 <?php
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\DebugLogger; // Adicione essa linha
 
 require  __DIR__ . "/../vendor/autoload.php";
 
 $mail = new PHPMailer(true);
 
 $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+$mail->Debugoutput = 'html'; // Adicione essa linha para habilitar a saída de depuração em HTML
 
 $mail->isSMTP();
 $mail->SMTPAuth = true;
 
-$mail->Host = "smtp.example.com";
+$mail->Host = "smtp.gmail.com";
 $mail->SMTPSecure = PHPMailer :: ENCRYPTION_STARTTLS;
 $mail->Port = 587;
 $mail->Username = "your-user@example.com";
