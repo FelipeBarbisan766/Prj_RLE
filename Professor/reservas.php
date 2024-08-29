d4ce5ddfa16cc2b27146da62c16ef67b3264b455
felipegalati <felipegalati20@gmail.com>
2024-08-29 18:16:00 -0300

    done: ordem de reservas

--- Professor/reservas.php
+++ Professor/reservas.php
@@ -26,7 +26,7 @@ date_default_timezone_set('America/Sao_Paulo');
  
     <?php
         $cod = $_SESSION['cod'];
-        $slq_reserva = mysqli_query($conexao, "SELECT r.res_aula as aula,r.res_desc as descr,r.res_isActive as active, l.lab_nome as lab, r.res_data as dat FROM reserva as r INNER JOIN laboratorio as l on r.lab_cod=l.lab_cod WHERE r.prof_cod='$cod' ORDER BY r.res_aula ASC");
+        $slq_reserva = mysqli_query($conexao, "SELECT r.res_aula as aula,r.res_desc as descr,r.res_isActive as active, l.lab_nome as lab, r.res_data as dat FROM reserva as r INNER JOIN laboratorio as l on r.lab_cod=l.lab_cod WHERE r.prof_cod='$cod' ORDER BY r.res_data DESC");
         $quantidade = $slq_reserva->num_rows;
         if ($quantidade >= 1){
             while ($reserva = mysqli_fetch_array($slq_reserva)) {
