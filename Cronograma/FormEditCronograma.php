3a279d0ff32b6485de137042a9a895c7cb233ca3
Eduardo Vizicato <eduardovizicato@gmail.com>
2024-09-09 17:52:00 -0300

    começando a tela de editar Cronograma e criando novos botões

--- Cronograma/FormEditCronograma.php
+++ Cronograma/FormEditCronograma.php
@@ -7,8 +7,7 @@
 </head>
 <body>
 <?php
-include_once ("../../navbar2.php");
-include_once ('../protectAdm.php');
+include_once ("../navbar2.php");
 ?>
 <div class="">
     
@@ -19,61 +18,148 @@ include_once ('../protectAdm.php');
         </svg>
         </a>
     </div>
-    <?php if(!isset($_GET['prof'])) { ?>
 
-    <h1 class="mb-4 text-3xl text-center mt-2 font-extrabold tracking-tight leading-none text-gray-900 md:text-3xl lg:text-4xl dark:text-white">Editar Professor</h1>
+    <h1 class="mb-4 text-3xl text-center mt-2 font-extrabold tracking-tight leading-none text-gray-900 md:text-3xl lg:text-4xl dark:text-white">Editar Cronograma</h1>
 
-    <form class="max-w-sm mx-auto" action="FormEditCronograma.php" method="GET">
-        <div class="mb-5">
-            <label for="prof" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Professor</label>
-            <select id="prof" name="prof" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                <!-- <?php
-                $slq = mysqli_query($conexao, "SELECT * FROM professor");
-                while ($prof = mysqli_fetch_array($slq)) {
-                    if ($prof['prof_isActive'] == true) { ?>
-                        <option value="<?php echo $prof['prof_cod']; ?>"><?php echo $prof['prof_nome']; ?></option>
-                        <?php }
-                }
-                ; ?> ARRUMAR PARA DELETAR -->
-            </select>
-        </div>
-        <button class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Selecionar</button>
-    </form>
+    <?php
+    include_once ("../conexao.php");
+    include_once ("../navbar2.php");
+    date_default_timezone_set('America/Sao_Paulo');
+?>
 
+<!-- TABELA 2 -->
+<?php
+if (isset($_GET['lab'])) {
+    $lab = $_GET['lab'];
+} else {
+    $labs = mysqli_fetch_array(mysqli_query($conexao, "SELECT * FROM laboratorio"));//tentar sumir com isso pq repete 
+    $lab = $labs['lab_cod'];
+}
+$labnome = mysqli_fetch_array(mysqli_query($conexao, "SELECT * FROM laboratorio WHERE lab_cod='$lab'"));
+$nomelab = $labnome["lab_nome"];
+
+?>
+
+<div class="container mx-auto px-4">
+
+    <?php
+    $slq = mysqli_query($conexao, "SELECT * FROM laboratorio");
+    ?>
     
-    <?php }else{?>
-        
-        <form class="max-w-sm mx-auto" action="altProf.php" method="POST">
-        <div class="mb-5 mt-2">
-                <?php
-                $cod = $_GET['prof'];
-                $slq = mysqli_query($conexao, 'SELECT * FROM professor WHERE prof_cod=' . $cod . '');
-                $prof = mysqli_fetch_array($slq);
-                echo '<input type="hidden" name="cod" value="'.$cod.'">';
-                ?>
-                <label for="nome" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nome do professor</label>
-                <input value="<?php echo $prof['prof_nome']; ?>" type="text" id="nome" name="nome" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Nome" required />
-            </div>
-            <div class="mb-5">
-                <label for="senha" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Senha para professor</label>
-                <input value="<?php echo $prof['prof_senha']; ?>" type="password" id="senha" name="senha" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
-            </div>
-            <div class="mb-5">
-            <label for="senha" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Cargo</label>
-            <select id="cargo" name="cargo" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                <option value="prof">Professor</option>
-                <option value="adm">Coordenador / Administrador</option>
-            </select>
+    <form class="max-w-sm mx-auto mb-3 mt-2" >  
+        <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Local</label>
+        <select id="countries" name="lab" onchange="status_update(this.options[this.selectedIndex].value)" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            <?php
+            include_once ("../conexao.php");
             
-            </div>
-            <div class="flex-row mb-5 sm:space-x-2">    
-                    <button href="pageControl.php" class="mb-2 text-white bg-danger-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">Cancelar</button>
-                    <button type="submit" class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Confirmar</button>
-            </div>
-        </div>
-        </form>
+            while ($labs = mysqli_fetch_array($slq)) {
+                if ($labs['lab_isActive'] == true) { 
+                    if(isset($_GET['lab'])){
+                        if($_GET['lab'] != $labs['lab_cod']){
+                            echo '<option value='.$labs['lab_cod'].'>'.$labs['lab_nome'].'</option>';
+                        }else{
+                            echo '<option value='.$_GET['lab'].' Selected>'.$nomelab.'</option>';
+                        }
+                    }else{
+                        echo '<option value='.$labs['lab_cod'].'>'.$labs['lab_nome'].'</option>';
+                    }
+                }}; ?>
+        </select>
+    
+             <script type="text/javascript">  
+            function status_update(value){  
+           let url = "cronograma2.php";  
+           window.location.href= url+"?lab="+value;  
+        }  
+        </script>  
+    </form>
+    
 
-    <?php } ?>    
+    <div class="relative overflow-x-auto sm:rounded-lg">
+        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                    <th scope="col" class="px-6 py-3"></th>
+
+                    <th scope="col" class="px-6 py-3">
+                        Segunda
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Terça
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Quarta
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Quinta
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Sexta
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+            <?php
+
+            for ($aula = 1; $aula <= 6; $aula++) {
+    
+            // echo $dia;
+            $slq = mysqli_query($conexao, "SELECT c.cro_aula as aula,c.cro_desc as descr,c.cro_sem as sem,c.cro_isActive as active, p.prof_nome as prof FROM cronograma as c INNER JOIN laboratorio as l on c.lab_cod=l.lab_cod INNER JOIN professor as p on c.prof_cod=p.prof_cod WHERE c.cro_aula = '$aula' AND c.lab_cod='$lab' ORDER BY c.cro_aula ASC");
+            while ($crono = mysqli_fetch_array($slq)) {
+                    switch ($crono["sem"]) {
+                        case "1":
+                            $seg = $crono['descr'];
+                            break;
+                        case "2":
+                            $ter = $crono['descr'];
+                            break;
+                        case "3":
+                            $qua = $crono['descr'];
+                            break;
+                        case "4":
+                            $qui = $crono['descr'];
+                            break;
+                        case "5":
+                            $sex = $crono['descr'];
+                            break;
+                        default:
+                            break;
+                        }
+                }
+
+                ; ?>
+                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                    <th scope="row" class="px-6 py-4 font-medium whitespace-nowrap text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400" >
+                        <?php echo $aula;?>ª Aula
+                    </th>
+                    <td class="px-6 py-4">
+                        <?php if(!isset($seg)){echo "Livre ";}else{echo $seg;}?>
+                    </td>
+                    <td class="px-6 py-4">
+                        <?php if(!isset($ter)){echo "Livre ";}else{echo $ter;}?>
+                    </td>
+                    <td class="px-6 py-4">
+                        <?php if(!isset($qua)){echo "Livre ";}else{echo $qua;}?>
+                    </td>
+                    <td class="px-6 py-4">
+                        <?php if(!isset($qui)){echo "Livre ";}else{echo $qui;}?>
+                    </td>
+                    <td class="px-6 py-4">
+                        <?php if(!isset($sex)){echo "Livre ";}else{echo $sex;}?>
+                    </td>
+                </tr>
+                <?php
+                $seg = null;
+                $ter = null;
+                $qua = null;
+                $qui = null;
+                $sex = null;
+                }?> 
+                
+            </tbody>
+        </table>
+    </div>
+    <div>
 </div>
     
 </body>
