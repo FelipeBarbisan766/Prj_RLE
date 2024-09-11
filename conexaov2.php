998d6b6f747c2214c23dd67eb6d86cc79e2abc98
FelipeBarbisan766 <felipebarbisan766@gmail.com>
2024-09-11 20:57:16 -0300

    update in conexao

--- conexaov2.php
+++ conexaov2.php
@@ -1,6 +1,10 @@
-
 <?php
-$con = mysqli_init();
-mysqli_ssl_set($con,NULL,NULL, "{path to CA cert}", NULL, NULL);
-mysqli_real_connect($conn, "fgatechnology.mysql.database.azure.com", "felipe", "!Barbisan2207!", "prj-rle", 3306, MYSQLI_CLIENT_SSL);
+
+$hostname = "srv487.hstgr.io";
+$dbname = "u410213008_rle";
+$username = "u410213008_rle";
+$password = "@Rle2024@";
+
+$conexao = mysqli_connect($hostname,$username,$password,$dbname);
+
 ?>
