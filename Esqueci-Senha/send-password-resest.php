6666d56f98538f251fa6f944c7f49813e692e695
FelipeBarbisan766 <felipebarbisan766@gmail.com>
2024-09-11 17:38:12 -0300

    update

--- Esqueci-Senha/send-password-resest.php
+++ Esqueci-Senha/send-password-resest.php
@@ -3,32 +3,32 @@ require_once '../conexao.php';
 
 $prof_nome = $_POST["email"];
 
-// $token = bin2hex(random_bytes(16));
+$token = bin2hex(random_bytes(16));
 
-// $token_hash = hash("sha256", $token);
+$token_hash = hash("sha256", $token);
 
-// $expiry = date("Y-m-d H:i:s", time() + 60 * 30);
+$expiry = date("Y-m-d H:i:s", time() + 60 * 30);
 
-// $sql = "SHOW TABLES LIKE 'professor'";
-//     $result = $conexao->query($sql);
+$sql = "SHOW TABLES LIKE 'professor'";
+    $result = $conexao->query($sql);
 
-//     if ($result->num_rows > 0) {
+    if ($result->num_rows > 0) {
         
-//         $sql = "UPDATE professor SET reset_token_hash = ?, reset_token_expires_at = ? WHERE prof_nome = ?";
+        $sql = "UPDATE professor SET reset_token_hash = ?, reset_token_expires_at = ? WHERE prof_nome = ?";
 
-//         $stmt = $conexao->prepare($sql);
-//         $stmt->bind_param("sss", $token_hash, $expiry, $prof_nome);
+        $stmt = $conexao->prepare($sql);
+        $stmt->bind_param("sss", $token_hash, $expiry, $prof_nome);
 
-//         if ($stmt->execute()) {
-//             echo "E-mail enviado com sucesso!";
-//         } else {
-//             echo "Erro ao enviar e-mail.";
-//         }
-//     } else {
-//         echo "A tabela professor não existe.";
-//     }
+        if ($stmt->execute()) {
+            echo "E-mail enviado com sucesso!";
+        } else {
+            echo "Erro ao enviar e-mail.";
+        }
+    } else {
+        echo "A tabela professor não existe.";
+    }
 
-// if($conexao->affected_rows){
+if($conexao->affected_rows){
     require __DIR__ . "/mailer.php";
 
     $mail->setFrom("fgatechtest@gmail.com");
@@ -44,6 +44,6 @@ $prof_nome = $_POST["email"];
     catch (Exception $e){
         echo "A mensagem não foi enviada. Mailer error {$mail->ErrorInfo}";
     }
-// }
+}
 
 echo "A Mensagem foi enviada, verifique seu email";
