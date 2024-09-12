3e9fa20fb3caa596b83a59a33d29d63a72338064
FelipeBarbisan766 <felipebarbisan766@gmail.com>
2024-09-12 11:04:50 -0300

    fix

--- Professor/reservas.php
+++ Professor/reservas.php
@@ -5,11 +5,11 @@ include_once ("../protect.php");
 date_default_timezone_set('America/Sao_Paulo');
 ?>
 <div class="px-4 mx-auto max-w-screen-xl ">    
-<a href="../index.php" class="my-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">         
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-    <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
-    </svg>
-    </a>
+<a href="../index.php" class="my-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+        </svg>
+        </a>
 </div>
 <div class=" py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
 <div class="relative overflow-x-auto">
