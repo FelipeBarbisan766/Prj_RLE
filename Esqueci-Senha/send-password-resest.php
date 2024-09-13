8d372e90c8a78da8377126746dde08a82b0f4c10
felipegalati <felipegalati20@gmail.com>
2024-09-13 10:26:12 -0300

    fix

--- Esqueci-Senha/send-password-resest.php
+++ Esqueci-Senha/send-password-resest.php
@@ -35,33 +35,27 @@ $mail->msgHTML("<h1>Para redefinir sua senha clique no link abaixo</h1>
                         <p>Caso você não tenha pedido para redefinir a senha, ignore esta mensagem</p>
 ");
 
-$mail->send();
-echo '<div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
+	if($mail->send());{
 
-	<div class="flex items-center p-4 mb-4 text-sm text-green-800 border border-green-300 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400 dark:border-green-800" role="alert">
-	<svg class="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+		echo '<div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
+		
+		<div class="flex items-center p-4 mb-4 text-sm text-green-800 border border-green-300 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400 dark:border-green-800" role="alert">
+		<svg class="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
 		<path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
-	</svg>
-	<span class="sr-only">Info</span>
-	<div>
+		</svg>
+		<span class="sr-only">Info</span>
+		<div>
 		<span class="font-medium">Sucesso!</span> Um email para redefinição de senha foi enviado!
-	</div>
-	</div>
-      
-    <a href="../login.php" class="my-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">         
-    Voltar
-    </a>
-	</div>
-      ';  
+		</div>
+		</div>
+		
+		<a href="../index.php" class="my-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">         
+		Voltar
+		</a>
+		</div>
+		';  
+		}
 
- }   
-	if(!empty($server_imap)) {
-		// Add the message to the IMAP.Sent mailbox
-		$mail_string = $mail->getSentMIMEMessage();
-		$imap_stream = imap_open("{".$server_imap."}", $email_account, $email_password);
-		imap_append($imap_stream, "{".$server_imap."}INBOX.Sent", $mail_string);
 
-        
-	}
 
 ?>
