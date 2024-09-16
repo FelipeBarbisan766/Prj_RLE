b3b59d6e861b8772f868ec13869d33df332e90a1
Eduardo Vizicato <eduardovizicato@gmail.com>
2024-09-16 16:13:52 -0300

    Creating FormDelCrono FormEditCrono

--- Cronograma/FormDelCronograma.php
+++ Cronograma/FormDelCronograma.php
@@ -1,29 +1,29 @@
 <?php
-include_once ("../../navbar2.php");
-include_once ('../protectAdm.php');
+include_once ("../navbar2.php");
+include_once ('../ADM/protectAdm.php');
 ?>
 
 <div class="container mx-auto px-4">
 
     <div class="mx-auto max-w-screen-xl ">
-        <a href="pageProf.php" class="my-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">         
+        <a href="cronograma2.php" class="my-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">         
         <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 8 14">    
           <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 1 1.3 6.326a.91.91 0 0 0 0 1.348L7 13"/>
         </svg>
     </a>
     </div>
 
-    <h1 class="mb-4 text-3xl text-center mt-2 font-extrabold tracking-tight leading-none text-gray-900 md:text-3xl lg:text-4xl dark:text-white">Desativar Professor</h1>
+    <h1 class="mb-4 text-3xl text-center mt-2 font-extrabold tracking-tight leading-none text-gray-900 md:text-3xl lg:text-4xl dark:text-white">Deletar aula</h1>
 
     <form class="max-w-sm mx-auto" method="POST">
         <div class="mb-5">
-            <label for="prof" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Selecionar Professor</label>
-            <select id="prof" name="prof" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            <label for="aula_crono" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Selecionar Aula</label>
+            <select id="aula_crono" name="aula_crono" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                 <?php
-                $slq = mysqli_query($conexao, "SELECT * FROM professor");
-                while ($prof = mysqli_fetch_array($slq)) {
-                    if ($prof['prof_isActive'] == true) { ?>
-                        <option value="<?php echo $prof['prof_cod'].' codeProf='.$prof['prof_cod']; ?>"><?php echo $prof['prof_nome']; ?></option>
+                $slq = mysqli_query($conexao, "SELECT * FROM cronograma");
+                while ($crono = mysqli_fetch_array($slq)) {
+                    if ($crono['cro_isActive'] == true) { ?>
+                        <option value="<?php echo $crono['cro_cod'].' codeCrono='.$crono['cro_cod']; ?>"> <?php echo $crono['cro_desc'];?> </option>
                         <?php }
                 }
                 ; ?>
@@ -49,9 +49,9 @@ include_once ('../protectAdm.php');
                 </svg>
                 <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400" id="modalTitle">Deseja desativar ?</h3>
                 
-                    <input type="hidden" name="cod" id="prof-form">
+                    <input type="hidden" name="cod" id="crono-form">
                 
-                <button onclick="deletarProfessor()" id="deactivate-btn" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">
+                <button onclick="deletarAula()" id="deactivate-btn" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">
                     Desativar
                 </button>
                 <button data-modal-hide="popup-modal" type="button" class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Cancelar</button>
@@ -61,12 +61,12 @@ include_once ('../protectAdm.php');
 </div>
 <script>
     //ESTÁ FUNCIONANDO, NÃO MEXER
-    function deletarProfessor() {
-  var profId = $('#prof').val().split('codeProf=')[1];
+    function deletarAula() {
+  var profId = $('#aula_crono').val().split('codeCrono=')[1];
   $.ajax({
     type: 'POST',
-    url: 'delProf.php',
-    data: { cod: profId },
+    url: 'delCrono.php',
+    data: { cod: cronoId },
     success: function() {
         $('[data-modal-hide="popup-modal"]').click();
         console.log('deu certo');
---
+++ Cronograma/FormEditCrono.php
@@ -0,0 +1,64 @@
+<?php
+include_once ("../navbar2.php");
+include_once ('../ADM/protectAdm.php');
+?>
+<div class="">
+    
+    <div class="px-4 mx-auto max-w-screen-xl ">
+        <a href="cronograma2.php" class="my-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">         
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+        </svg>
+        </a>
+    </div>
+    <?php if(!isset($_GET['crono'])) { ?>
+
+    <h1 class="mb-4 text-3xl text-center mt-2 font-extrabold tracking-tight leading-none text-gray-900 md:text-3xl lg:text-4xl dark:text-white">Editar Aula</h1>
+
+    <form class="max-w-sm mx-auto" action="FormEditCrono.php" method="GET">
+        <div class="mb-5">
+            <label for="crono" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Aula</label>
+            <select id="crono" name="crono" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <?php
+                $slq = mysqli_query($conexao, "SELECT * FROM cronograma");
+                while ($crono = mysqli_fetch_array($slq)) {
+                    if ($crono['cro_isActive'] == true) { ?>
+                        <option value="<?php echo $crono['cro_cod']; ?>"><?php echo $crono['cro_desc']; ?></option>
+                        <?php }
+                }
+                ; ?>
+            </select>
+        </div>
+        <button class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Selecionar</button>
+    </form>
+
+    
+    <?php }else{?>
+        
+        <form class="max-w-sm mx-auto" action="altProf.php" method="POST">
+        <div class="mb-5 mt-2">
+                <?php
+                $cod = $_GET['crono'];
+                $slq = mysqli_query($conexao, 'SELECT * FROM cronograma WHERE cro_cod=' . $cod . '');
+                $prof = mysqli_fetch_array($slq);
+                echo '<input type="hidden" name="cod" value="'.$cod.'">';
+                ?>
+                <label for="nome" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nome da aula</label>
+                <input value="<?php echo $crono['cro_desc']; ?>" type="text" id="cro_desc" name="cro_desc" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Nome" required />
+            </div>
+            <div class="mb-5">
+                <label for="senha" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Para que aula deseja mudar ?</label>
+                <input value="<?php echo $crono['cro_aula']; ?>" type="" id="cro_aula" name="cro_aula" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+            </div>
+            <div class="flex-row mb-5 sm:space-x-2">    
+                    <button href="cronograma2.php" class="mb-2 text-white bg-danger-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">Cancelar</button>
+                    <button type="submit" class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Confirmar</button>
+            </div>
+        </div>
+        </form>
+
+    <?php } ?>    
+</div>
+
+</body>
+</html>
