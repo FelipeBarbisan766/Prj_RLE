119166a157f38a227959e2f76dfb41d7cf6952f1
FelipeBarbisan766 <felipebarbisan766@gmail.com>
2024-09-23 09:00:20 -0300

    updates

--- login.php
+++ login.php
@@ -11,12 +11,14 @@
 
 <body class="flex flex-col min-h-screen">
   <?php
-  include_once ("conexao.php");
-  include_once('../button_back.php');
+  include_once ("conexao.php");  
   ?>
+  
+
 
 <section class="">
   <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+ 
       <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
           RLE    
       </a>
