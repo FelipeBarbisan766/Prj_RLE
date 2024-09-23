2e989e080b819a5685ba17cd749d662f53954729
Eduardo Vizicato <eduardovizicato@gmail.com>
2024-09-23 15:56:22 -0300

    fixing calendar toolbar

--- Calendario/js/custom.js
+++ Calendario/js/custom.js
@@ -66,4 +66,18 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
   calendar.render();
+  var toolbarLeft = document.querySelector('.fc-toolbar .fc-toolbar-chunk:first-child');
+  toolbarLeft.innerHTML = `
+      <div class="inline-flex rounded-md shadow-sm float-right" role="group">
+          <a href="diaV3.php" type="button" class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-s-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-blue-500 dark:focus:text-white" selected>
+              Dia
+          </a>
+          <a href="semanaV2.php" type="button" class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border-t border-b border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-blue-500 dark:focus:text-white">
+              Semana
+          </a>
+          <a href="calendario.php" type="button" class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-e-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-blue-500 dark:focus:text-white" disabled>
+              Mês
+          </a>
+      </div>
+  `;
 });
