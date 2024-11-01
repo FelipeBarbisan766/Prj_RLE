8b027a8a2eccd97402d69036bc9bd1f4c337eec8
felipegalati <felipegalati20@gmail.com>
2024-11-01 19:57:29 -0300

    bugs

--- Reserva/formReserva.php
+++ Reserva/formReserva.php
@@ -37,11 +37,13 @@ option:disabled {
                 }
                 ; ?>            
             </select>
+            <!-- esse script está bugando o visual do select -->
             <script>
                 $(document).ready(function() {
                     $('#prof').select2();
                 });
             </script>
+            <!--  -->
             <label for="curso" class="flex px-28 mb-2 text-sm font-medium text-gray-900 dark:text-white text-4xl">Curso</label>
             <select name="curso" id="curso" class="form-select block mb-2 font-medium dark:text-white bg-gray-50 border border-gray-300 text-gray-900 mb-6 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                 <?php
@@ -173,6 +175,7 @@ option:disabled {
             <option value="3">Noite</option>
         </select>
     </div>
+    <!-- botao nao ta aparecendo no meu pc (felipao) -->
   <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Continuar</button>
 </form>
 <?php }; ?>
