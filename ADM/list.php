878ccc7c73833407f62b663df00b066288ee62d6
felipegalati <felipegalati20@gmail.com>
2024-11-05 18:23:41 -0300

    .

--- ADM/list.php
+++ ADM/list.php
@@ -59,7 +59,7 @@ window.location.href= url+"?filtro="+value;
 <br><br>
 
 
-    <div class="relative overflow-x-auto sm:rounded-lg">
+    <div class="relative overflow-x-auto sm:rounded-lg mb-2">
         
         <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
             <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
