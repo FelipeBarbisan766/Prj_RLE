22818a45b92f440c12740bef5e8714cf65e4c8d6
Eduardo Vizicato <eduardovizicato@gmail.com>
2024-11-05 18:34:15 -0300

    some fixes

--- Reserva/formReserva.php
+++ Reserva/formReserva.php
@@ -191,7 +191,7 @@ option:disabled {
         </select>
     </div>
     <!-- botao nao ta aparecendo no meu pc (felipao) -->
-  <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Continuar</button>
+  <button type="submit" class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Continuar</button>
 </form>
 <?php }; ?>
     </div>
