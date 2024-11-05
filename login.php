50e03665f3a26d84ab747e061e6d6bf1d90712bd
felipegalati <felipegalati20@gmail.com>
2024-11-05 18:03:59 -0300

    visual login

--- login.php
+++ login.php
@@ -9,7 +9,7 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
-<body class="bg-gray-200 dark:bg-gray-900 flex flex-col min-h-screen">
+<body class="bg-red-700 flex flex-col min-h-screen">
   <?php
   include_once ("conexao.php");  
   ?>
@@ -19,7 +19,7 @@
 <section class="">
   <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
  
-      <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+      <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-white">
           RLE    
       </a>
       <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
--- navbar2.php
+++ navbar2.php
@@ -44,7 +44,7 @@
             </div>
             </a>';
         }else{
-            echo '<a href="\Prj_RLE/login.php" class="block py-2 px-3 text-gray-900 bg-neutral-200 border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Entrar</a>';
+            echo '<a href="\Prj_RLE/login.php" class="block py-2 px-3 text-gray-900 bg-neutral-100 border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Entrar</a>';
         }
         
         ?>
